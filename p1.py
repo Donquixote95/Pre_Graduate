b82fe53d0788cae7dece421621ad2cc46d6fe821
LimDoeon <doeonlim8656@gmail.com>
2023-01-14 13:44:39 +0900

    v2

--- p1.py
+++ p1.py
@@ -1,15 +1,28 @@
 # 1. 숫자의 종류 세기
 numbers = list((input("integers>")))
 counter = {}
+# ex ; 123412314123
+
 # print(numbers)
 # for i in range(len(numbers)):
 #     print("i".isdigit()) # 결과 : False
 for i in range(len(numbers)):
     numbers[i] = int(numbers[i])
 # print(numbers)
+
+# 내가 작성
+""" 
 for number in numbers:
     if number not in counter:
         counter[number] = 1
-    else:
+     else:
         counter[number] = counter[number] + 1
+"""
+
+# 답지 버전
+for number in numbers:
+    if number not in counter:
+        counter[number] = 0
+    counter[number] += 1
+    
 print(counter)
