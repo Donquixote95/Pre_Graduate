2f4f004bd3248a6e8353c2d4b34bf9e2f961a28d
LimDoeon <doeonlim8656@gmail.com>
2023-01-14 11:07:27 +0900

    v1

---
+++ p2.py
@@ -0,0 +1,12 @@
+# 2. 염기의 개수 세기
+blue_print = input("dna_blue_print>")
+
+a_blue = blue_print.count("a")
+t_blue = blue_print.count("t")
+g_blue = blue_print.count("g")
+c_blue = blue_print.count("c")
+
+print("a의 개수:", a_blue)
+print("t의 개수:", t_blue)
+print("g의 개수:", g_blue)
+print("c의 개수:", c_blue)
